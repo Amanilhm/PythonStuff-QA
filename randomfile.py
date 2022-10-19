f02cefc1388c608f7f7f3136af439fdc5e18e8ef
Amani Lahmami <amani.lahmami@avanade.com>
2022-10-19 16:30:28 +0100

    exercise 1 uploaded

--- randomfile.py
+++ randomfile.py
@@ -1,1 +1,7 @@
-print("Hello World")
+english = float(input("Please enter your English marks: "));
+maths = float(input("Please enter your Maths marks: "));
+science = float(input("Please enter your Science marks: "));
+total = english + maths + science;
+average = total/3;
+print("Your average score is:");
+print(average);
