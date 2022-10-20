35ca4e450af0db7feb77f1400cf3df2d2a5b4733
Amani Lahmami <amani.lahmami@avanade.com>
2022-10-20 10:09:54 +0100

    Temperature Gauge Exercise completed

---
+++ temperaturegauge.py
@@ -0,0 +1,9 @@
+temperature = int(input('Please input the temperature: '))
+if (temperature > 30):
+    print('too hot')
+    print('aagh')
+elif(temperature < 0):
+    print('AAH')
+    print('too cold')
+else:
+    print('perfect')
